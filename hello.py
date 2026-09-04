d39f1e9632740f52dde866fdcd558bf19f626ace
Hayley <hayleyytann@gmail.com>
2026-09-04 17:01:47 +0800

    Assignment variables

--- hello.py
+++ hello.py
@@ -19,3 +19,18 @@ print("Followers:", followers)
 ## activity 2 answers:
 ## a. they are storing data so i can reuse the value multiple times without retyping it.
 ## b. yes if i were to change the cool_creator to just creator, run it again, the output will print Username: creator
+
+## Activity 3 
+followers += 50
+print("day 1:", followers)
+
+followers += 20
+print("day 2:", followers)
+
+followers -= 10
+print("day 3:", followers)
+
+## activity 3 answers
+## a. no the += and -= does the reassignment for me
+## b. each operation builds on whatever followers currently holds not the original 100 thats why the numbers keep climbin
+## c. it updates a variable based on its current value 
