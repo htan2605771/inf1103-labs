3b26c247709fc0f905e01a32295d9b5ec8579578
Hayley <hayleyytann@gmail.com>
2026-09-04 16:45:59 +0800

    First Post

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("==============================================")
+print("Welcome here!")
+print("My first post!")
+print("==============================================")
+
+## a. Yes
+## b. top to bottom
+## c. the output appears in the python terminal
