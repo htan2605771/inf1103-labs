a2edb4012e9fc6b76162c4caf7593868742ccd67
Hayley <hayleyytann@gmail.com>
2026-09-04 16:57:28 +0800

    Added profile variables

--- hello.py
+++ hello.py
@@ -6,3 +6,16 @@ print("==============================================")
 ## a. Yes
 ## b. top to bottom
 ## c. the output appears in the python terminal
+
+## Activity 2
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+## activity 2 answers:
+## a. they are storing data so i can reuse the value multiple times without retyping it.
+## b. yes if i were to change the cool_creator to just creator, run it again, the output will print Username: creator
